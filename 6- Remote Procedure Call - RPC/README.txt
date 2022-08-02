772c94324cbddd57881d5bdd715ed46d17718b12
franco dreher <dreheragustin@hotmail.com>
2022-08-02 18:55:50 -0300

    rpc

---
+++ 6- Remote Procedure Call - RPC/README.txt
@@ -0,0 +1,7 @@
+Poner en funcionamiento el servidor RPC:
+	cd RPCServer
+	dotnet run
+
+Para hacer la request de un numero fibonacci, correr el cliente:
+	cd RpcClient
+	dotnet run
---
+++ 6- Remote Procedure Call - RPC/RpcClient/Program.cs
@@ -0,0 +1,7 @@
+﻿var rpcClient = new RpcClient();
+
+Console.WriteLine(" [x] Requesting fib(30)");
+var response = rpcClient.Call("30");
+
+Console.WriteLine(" [.] Got '{0}'", response);
+rpcClient.Close();
